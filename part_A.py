d87aec80afebe6702946bf006f4bd854f8272897
Herman Aarebrot <Herman.Arebrot@student.nhh.no>
2024-09-29 13:15:53 +0200

    updatet

--- part_A.py
+++ part_A.py
@@ -1,10 +1,15 @@
+import math as mt
+import numpy as np
+
+
+
 """
 TECH2 mandatory assignment - Part A
 
 Write the implementation of part A of the exercise below.
 """
 
-def std_loops(x):
+def std_loops(numbers):
     """
     Compute standard deviation of x using loops.
 
@@ -17,8 +22,37 @@ def std_loops(x):
     sd : float
         Standard deviation of the list of numbers.
     """
+    
+    
+    sum = 0
+    i = 0
+    
+    for num in numbers:
+        sum += num
+        i += 1 
+        
+    mean = sum/i
+    
+    
+    sumOfSquares = 0
+    for num in numbers: 
+        sumOfSquares += num**2
+        
+    S = sumOfSquares/i
+    
+    variencesquared = S - mean**2
+    
+    
+    
+    return mt.sqrt(variencesquared)
 
-def std_builtin(x):
+
+    
+    
+      
+        
+
+def std_builtin(numbers):
     """
     Compute standard deviation of x using the built-in functions sum()
     and len().
@@ -33,3 +67,24 @@ def std_builtin(x):
         Standard deviation of the list of numbers.
     """
     
+    mean = sum(numbers)/len(numbers)
+    
+    
+    S = sum([num**2 for num in numbers])/len(numbers)
+    
+    variance = S - mean**2
+    
+    return mt.sqrt(variance)
+
+
+# method 3
+
+
+
+
+test = [1,2,3]
+std = np.std(test)
+
+#print(std_builtin(test) == std_loops(test) == std)
+
+
