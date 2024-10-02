f84d0032a325e39b25a8acce622089b1655cca87
Herman Aarebrot <Herman.Arebrot@student.nhh.no>
2024-10-02 12:55:58 +0200

    Newest version

--- part_A.py
+++ part_A.py
@@ -1,6 +1,4 @@
-import math as mt
 import numpy as np
-
 
 
 """
@@ -9,15 +7,17 @@ TECH2 mandatory assignment - Part A
 Write the implementation of part A of the exercise below.
 """
 
-# -------------------------------------------- Functions -----------------------------------
+# ------------------------------------ Functions -----------------------------------
 
+
+# ------------- Method 1 ---------------
 def std_loops(numbers):
     """
     Compute standard deviation of x using loops.
 
     Parameters
     ----------
-    x: Sequence of numbers
+    numbers: Sequence of numbers
 
     Returns
     -------
@@ -25,29 +25,36 @@ def std_loops(numbers):
         Standard deviation of the list of numbers.
     """
     
+    # --- Code block to add find the mean of the sequece of numbers ---
+    sum = 0  # Variable to store all the "part" sum 
+    i = 0    # variable to count numbers
     
-    sum = 0
-    i = 0
-    
+    # itterate through every number in numbers and add it to the sum  
     for num in numbers:
-        sum += num
-        i += 1 
+        sum += num  #updating sum
+        i += 1    # updation numbers itterated through
         
+    #calculating the mean 
     mean = sum/i
+
+    # --- Code block to calculate the mean of squares
     
-    sumOfSquares = 0
+    sumOfSquares = 0 #variable to contain the sum of sqares of the number in the sequence numbers 
+    
+    # itterate through every number in numbers
     for num in numbers: 
-        sumOfSquares += num**2
+        sumOfSquares += num**2  #update the sum of squares
         
-    S = sumOfSquares/i
-    variencesquared = S - mean**2
+    S = sumOfSquares/i  # calcucate the mean of sqares
     
-    return mt.sqrt(variencesquared)
+    variencesquared = S - mean**2  # calcualte the variance
+    
+    # Returning the standard deviation using the sqrt(function) form numpy
+    return np.sqrt(variencesquared)
 
 
     
-    
-      
+# ------------- Metod 2 -------------
         
 
 def std_builtin(numbers):
@@ -65,25 +72,35 @@ def std_builtin(numbers):
         Standard deviation of the list of numbers.
     """
     
+    # calcualte the mean as the sum devided by the number of items in the list using inbuilt python functions
     mean = sum(numbers)/len(numbers)
+    
+    # calcualte the mean of squares as the sum of squared devided by number of numbers in the list
     S = sum([num**2 for num in numbers])/len(numbers)
+    
+    # Calcualte variance
     variance = S - mean**2
-    return mt.sqrt(variance)
+    # Returning the stander deviation
+    return np.sqrt(variance)
 
 
-# method 3
+# -------------- Method 3 -------------
+
+# A function using numpy's std function to find the standard deviation
 def std_numpy(numbers):
     return np.std(numbers) 
 
 
 
 def main(): 
-    #showingthe result is the same
+    # Demonstrating that the fuctions provide the same result for all the functions
     num_lst = [i for i in range(1,5)]
     print(std_builtin(num_lst) == std_loops(num_lst) == std_numpy(num_lst))
     
     
-# ----------------------------------- Main ---------------------------------
+    
+
+# --------------------------------------------- Main program -------------------------------------------
 
 main()
     
